9e873dd88409332402ab9e634709eee085dd8108
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-07-04 14:23:57 +0600

    demonstrated the use of constructor function and added two method to the prototype of Person.

---
+++ OOP/ConstructorFunction.js
@@ -0,0 +1,23 @@
+const Person = function (firstName, lastName, birthDate) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.birthDate = birthDate;
+};
+
+Person.prototype.calcAge = function () {
+  currentDate = new Date();
+  birthDate = new Date(this.birthDate);
+  return currentDate.getFullYear() - birthDate.getFullYear();
+};
+
+Person.prototype.printInfo = function () {
+  console.log(
+    `Name of the person: ${this.firstName} ${
+      this.lastName
+    }. His age is: ${this.calcAge()}`
+  );
+};
+
+const arka = new Person('Arka', 'Bhuiyan', '1997-09-01');
+
+arka.printInfo();
