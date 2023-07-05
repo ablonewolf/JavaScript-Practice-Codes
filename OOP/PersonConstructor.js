e58acb23b4e9b6234fbf4e0472196dd4b5cf5444
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-07-05 15:44:40 +0600

    demonstrated inheritance with the class of Student. Added one additon method in Person class named fullName.

--- OOP/PersonConstructor.js
+++ OOP/PersonConstructor.js
@@ -6,6 +6,10 @@ const Person = function (firstName, lastName, birthDate) {
 
 Person.prototype.gender = 'NotSet';
 
+Person.prototype.fullName = function () {
+  return `${this.firstName} ${this.lastName}`;
+};
+
 Person.prototype.calcAge = function () {
   currentDate = new Date();
   birthDate = new Date(this.birthDate);
@@ -20,7 +24,25 @@ Person.prototype.printInfo = function () {
   );
 };
 
+const Student = function (firstName, lastName, birthDate, course) {
+  Person.call(this, firstName, lastName, birthDate);
+  this.course = course;
+};
+
+// linking the students prototype with the prototype property of Person
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function () {
+  console.log(`Hi! I am ${this.fullName()}. My major is ${this.course}.`);
+};
+
 const arka = new Person('Arka', 'Bhuiyan', '1997-09-01');
 arka.gender = 'male';
 
 arka.printInfo();
+
+const arghya = new Student('Arghya', 'Bhuiyan', '1998-11-28', 'Economics');
+arghya.gender = 'male';
+
+arghya.introduce();
+arghya.printInfo();
