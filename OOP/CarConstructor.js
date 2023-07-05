e24f7a062c7023fd81ef55cefb1b04c7da816dea
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-07-05 10:04:44 +0600

    Created a Car object using Function constructor and added several methods in the prototype object of Car.

---
+++ OOP/CarConstructor.js
@@ -0,0 +1,32 @@
+const Car = function (brand, color, speed) {
+  this.brand = brand;
+  this.color = color;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 5;
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+};
+
+Car.prototype.printInfo = function () {
+  console.log(
+    `This is the car of the brand ${this.brand} and of the color ${this.color}. Currently it has a speed of ${this.speed} km/h.`
+  );
+};
+
+const BMW = new Car('BMW', 'black', 60);
+const Ford = new Car('Ford', 'blue', 50);
+
+BMW.accelerate();
+BMW.accelerate();
+BMW.brake();
+BMW.printInfo();
+
+Ford.brake();
+Ford.brake();
+Ford.accelerate();
+Ford.printInfo();
