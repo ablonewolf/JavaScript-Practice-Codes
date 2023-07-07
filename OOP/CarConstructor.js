223dd6c4d1115bb42cbb5b62d28d74b640016671
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-07-08 00:20:24 +0600

    reformatted the code for CarConstructor.js

--- OOP/CarConstructor.js
+++ OOP/CarConstructor.js
@@ -1,21 +1,21 @@
 const Car = function (brand, color, speed) {
-  this.brand = brand;
-  this.color = color;
-  this.speed = speed;
+    this.brand = brand;
+    this.color = color;
+    this.speed = speed;
 };
 
 Car.prototype.accelerate = function () {
-  this.speed += 5;
+    this.speed += 5;
 };
 
 Car.prototype.brake = function () {
-  this.speed -= 5;
+    this.speed -= 5;
 };
 
 Car.prototype.printInfo = function () {
-  console.log(
-    `This is the car of the brand ${this.brand} and of the color ${this.color}. Currently it has a speed of ${this.speed} km/h.`
-  );
+    console.log(
+        `This is the car of the brand ${this.brand} and of the color ${this.color}. Currently it has a speed of ${this.speed} km/h.`
+    );
 };
 
 const BMW = new Car('BMW', 'black', 60);
