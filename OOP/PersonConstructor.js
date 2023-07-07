105341f44ac4513f85f63bdb11e520877d2433a7
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-07-08 00:21:59 +0600

    reformatted the code for PersonConstructor.js

--- OOP/PersonConstructor.js
+++ OOP/PersonConstructor.js
@@ -1,32 +1,32 @@
 const Person = function (firstName, lastName, birthDate) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.birthDate = birthDate;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthDate = birthDate;
 };
 
 Person.prototype.gender = 'NotSet';
 
 Person.prototype.fullName = function () {
-  return `${this.firstName} ${this.lastName}`;
+    return `${this.firstName} ${this.lastName}`;
 };
 
 Person.prototype.calcAge = function () {
-  currentDate = new Date();
-  birthDate = new Date(this.birthDate);
-  return currentDate.getFullYear() - birthDate.getFullYear();
+    currentDate = new Date();
+    birthDate = new Date(this.birthDate);
+    return currentDate.getFullYear() - birthDate.getFullYear();
 };
 
 Person.prototype.printInfo = function () {
-  console.log(
-    `Name of the person: ${this.firstName} ${
-      this.lastName
-    }; age is: ${this.calcAge()}, with the sex of ${this.gender}.`
-  );
+    console.log(
+        `Name of the person: ${this.firstName} ${
+            this.lastName
+        }; age is: ${this.calcAge()}, with the sex of ${this.gender}.`
+    );
 };
 
 const Student = function (firstName, lastName, birthDate, course) {
-  Person.call(this, firstName, lastName, birthDate);
-  this.course = course;
+    Person.call(this, firstName, lastName, birthDate);
+    this.course = course;
 };
 
 // linking the students prototype with the prototype property of Person
@@ -34,7 +34,7 @@ Student.prototype = Object.create(Person.prototype);
 Student.prototype.constructor = Student;
 
 Student.prototype.introduce = function () {
-  console.log(`Hi! I am ${this.fullName()}. My major is ${this.course}.`);
+    console.log(`Hi! I am ${this.fullName()}. My major is ${this.course}.`);
 };
 
 const arka = new Person('Arka', 'Bhuiyan', '1997-09-01');
