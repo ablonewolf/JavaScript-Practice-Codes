ad9e8fa5beaf32cb4650bca844775a35e5b385a4
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-07-08 00:21:32 +0600

    reformatted the code for PersonClass.js

--- OOP/PersonClass.js
+++ OOP/PersonClass.js
@@ -1,56 +1,56 @@
 class Person {
-  constructor(firstName, lastName, birthDate) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.birthDate = birthDate;
-  }
+    constructor(firstName, lastName, birthDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
 
-  set gender(gender) {
-    this._gender = gender;
-  }
+    set gender(gender) {
+        this._gender = gender;
+    }
 
-  get gender() {
-    return this._gender;
-  }
+    get gender() {
+        return this._gender;
+    }
 
-  calcAge() {
-    let currentDate = new Date();
-    let birthDate = new Date(this.birthDate);
-    return currentDate.getFullYear() - birthDate.getFullYear();
-  }
+    calcAge() {
+        let currentDate = new Date();
+        let birthDate = new Date(this.birthDate);
+        return currentDate.getFullYear() - birthDate.getFullYear();
+    }
 
-  get fullName() {
-    return `${this.firstName} ${this.lastName}`;
-  }
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
 
-  printInfo() {
-    console.log(
-      `Name of the person: ${this.firstName} ${
-        this.lastName
-      }; age is: ${this.calcAge()}, sex is: ${this.gender}.`
-    );
-  }
+    printInfo() {
+        console.log(
+            `Name of the person: ${this.firstName} ${
+                this.lastName
+            }; age is: ${this.calcAge()}, sex is: ${this.gender}.`
+        );
+    }
 
-  static greetings() {
-    console.log(`Greetings!!`);
-  }
+    static greetings() {
+        console.log(`Greetings!!`);
+    }
 }
 
 class Student extends Person {
-  constructor(firstName, lastName, birthDate, subject) {
-    super(firstName, lastName, birthDate);
-    this.subject = subject;
-  }
+    constructor(firstName, lastName, birthDate, subject) {
+        super(firstName, lastName, birthDate);
+        this.subject = subject;
+    }
 
-  printInfo() {
-    console.log(
-      `Name of the student is: ${
-        this.fullName
-      }, age is: ${this.calcAge()}, sex is: ${this.gender} and the major is: ${
-        this.subject
-      }`
-    );
-  }
+    printInfo() {
+        console.log(
+            `Name of the student is: ${
+                this.fullName
+            }, age is: ${this.calcAge()}, sex is: ${this.gender} and the major is: ${
+                this.subject
+            }`
+        );
+    }
 }
 
 const arka = new Person('Arka', 'Bhuiyan', '1997-09-01');
@@ -60,10 +60,10 @@ arka.printInfo();
 console.log(`Arka's Full name is: ${arka.fullName}`);
 
 const student = new Student(
-  'Arka',
-  'Bhuiyan',
-  '1997-09-01',
-  'Computer Science and Engineering'
+    'Arka',
+    'Bhuiyan',
+    '1997-09-01',
+    'Computer Science and Engineering'
 );
 student.gender = 'male';
 Person.greetings();
