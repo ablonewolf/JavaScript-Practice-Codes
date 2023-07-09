a381702dfd88615157c42d584e20027c8a6b86d7
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-07-09 13:10:12 +0600

    rendered the country data in a beautiful html format.

--- AsynchronousJavaScriptAndPromises/script.js
+++ AsynchronousJavaScriptAndPromises/script.js
@@ -1,16 +1,35 @@
-'use strict'
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
 const getCountryData = function (countryName) {
-    const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${countryName}`);
+  request.send();
 
-    const request = new XMLHttpRequest();
-    request.open('GET', `https://restcountries.com/v3.1/name/${countryName}`);
-    request.send();
-
-    request.addEventListener('load', function () {
-        const [data] = JSON.parse(this.responseText);
-        console.log(data);
-    })
-}
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    const html = `<article class="country">
+          <img class="country__img" src="${data.flags['png']}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 100000
+            ).toFixed(1)}M people</p>
+            <p class="country__row"><span>🗣️</span>${
+              Object.values(data.languages)[0]
+            }</p>
+            <p class="country__row"><span>💰</span>${
+              Object.keys(data.currencies)[0]
+            }</p>
+          </div>
+        </article>`;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+    console.log(data);
+  });
+};
 
 getCountryData('bangladesh');
 getCountryData('india');
