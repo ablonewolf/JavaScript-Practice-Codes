a6ed5171c3549110ca326615e899749f3c981438
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-07-16 11:25:19 +0600

    promisified the setTimeout function.

--- AsynchronousJavaScriptAndPromises/Promises.js
+++ AsynchronousJavaScriptAndPromises/Promises.js
@@ -12,3 +12,19 @@ const lotteryPromise = new Promise((resolve, reject) => {
 lotteryPromise
   .then((res) => console.log(res))
   .catch((err) => console.error(err));
+
+// Promisifying setTimeout function
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds);
+  });
+};
+
+wait(2)
+  .then(() => {
+    console.log(`I waited for 2 seconds.`);
+    return wait(3);
+  })
+  .then(() => {
+    console.log(`I waited for 3 seconds.`);
+  });
